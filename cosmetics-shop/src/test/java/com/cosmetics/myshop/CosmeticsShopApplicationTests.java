e526e8fbc298112f07ee1f422f0d287686fb41d6
tay <phuonggteyy@gmail.com>
2024-03-16 08:28:38 +0700

    init project

---
+++ cosmetics-shop/src/test/java/com/cosmetics/myshop/CosmeticsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmetics.myshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
