9cf6ac231eb5026a046dd9b0cbeb9e6d33a90a0b
21120589 <truonganhtuan3092@gmail.com>
2024-03-24 15:50:54 +0700

    adjust string utils

--- cosmetics-shop/src/main/java/com/cosmetics/myshop/utils/StringUtils.java
+++ cosmetics-shop/src/main/java/com/cosmetics/myshop/utils/StringUtils.java
@@ -1,8 +1,24 @@
 package com.cosmetics.myshop.utils;
 
 public class StringUtils {
+	
+	public StringUtils() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	public static String normalizeString(String input) {
 		String result = input.trim().replaceAll("_", " ");
-		return result.toLowerCase();		
+		String[] words = result.split("\\s+");
+		result = "";
+		for (String word: words) {
+			String firstLetter = word.substring(0,1).toUpperCase();
+			result +=firstLetter;
+			String restOfWord = word.substring(1);
+			result +=restOfWord + " ";
+			
+		}
+		System.out.println(result);
+		return result;		
 	}
 }
